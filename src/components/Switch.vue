4f47693edf31bd6e8d5efe6709e5edea246289ec
王学龙 <2374146085@qq.com>
2022-01-15 22:44:56 +0800

    按钮UI初步形成

--- src/components/Switch.vue
+++ src/components/Switch.vue
@@ -1,59 +1,22 @@
 <template>
   <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
-  <div>switch</div>
+  <Switch v-model:value="checked"></Switch>
 </template>
 
 <script>
+import { ref } from 'vue'
+import Switch from '../lib/Switch.vue'
 export default {
+  components:{
+    Switch
+  },
+  setup(){
+    const checked = ref(true)
 
+    return{
+      checked
+    }
+  }
 }
 </script>
 
---
+++ src/lib/Switch.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <!-- 当value为true，button的class就是checked -->
+    <button :class="{ checked: value }" @click="toggle"><span></span></button>
+    <div>{{ value }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: Boolean,
+  },
+  setup(props, context) {
+    function toggle() {
+      context.emit('update:value', !props.value)
+    }
+    return {
+      toggle,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: rgb(116, 116, 116);
+  border-radius: $h/2;
+  position: relative;
+  > span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: left 250ms;
+  }
+&:focus {
+  outline: none;
+}
+&.checked {
+  background: rgb(0, 229, 255);
+  > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+}
+}
+</style>
