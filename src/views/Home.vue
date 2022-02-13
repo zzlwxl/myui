b67abb4050e4649c415de14bc8bee812497a9723
王学龙 <2374146085@qq.com>
2022-02-13 19:30:42 +0800

    首页布局优化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,30 +11,30 @@
       </div>
     </div>
     <div class="info">
-    <ul>
-      <li>
-        <svg>
-          <use xlink:href="#icon-vue"></use>
-        </svg>
-        <h3>基于 Vue 3</h3>
-        <p>为了学习vue3而写的一个组件库</p>
-      </li>
-      <li>
-        <svg>
-          <use xlink:href="#icon-js"></use>
-        </svg>
-        <h3>基于 JavaScript </h3>
-        <p>还是要学习TypeScript的</p>
-      </li>
-      
-      <li>
-        <svg>
-          <use xlink:href="#icon-idea"></use>
-        </svg>
-        <h3>代码易读</h3>
-        <p>代码简单</p>
-      </li>
-    </ul>
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>为了学习vue3而写的一个组件库</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-js"></use>
+          </svg>
+          <h3>基于 JavaScript</h3>
+          <p>还是要学习TypeScript的</p>
+        </li>
+
+        <li>
+          <svg>
+            <use xlink:href="#icon-idea"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>代码简单</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -54,47 +54,30 @@ export default {
   border-bottom-right-radius: 50% 40px;
   background: linear-gradient(124deg, rgba(2, 0, 36, 1) 0%, rgba(255, 15, 109, 1) 35%, rgba(0, 212, 255, 1) 100%);
 }
-.info{
+.info {
   margin: 64px auto;
   padding: 0 16px;
   @media (min-width: 800px) {
     width: 800px;
   }
+  > ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    > li {
+      width: 390px;
+      margin-bottom: 30px;
+      text-align: center;
+    }
+  }
   @media (min-width: 1200px) {
     width: 1200px;
-    >ul{
-      >li{
-        width: 30%;
+    > ul {
+      > li {
+        width: 300px;
       }
     }
   }
-  >ul {
-    display: flex;
-    flex-wrap: wrap;
-    >li {
-      width: 389px;
-      margin: 16px 0;
-      display: grid;
-      justify-content: start;
-      align-content: space-between;
-      grid-template-areas:
-        "icon title"
-        "icon text";
-      grid-template-columns: 80px auto;
-      grid-template-rows: 1fr auto;
-      >svg {
-        grid-area: icon;
-        width: 64px;
-        height: 64px;
-      }
-      >h3 {
-        grid-area: title;
-        font-size: 28px;
-      }
-      >p {
-        grid-area: text
-      }
-    }}
 }
 .banner {
   padding: 100px 0;
